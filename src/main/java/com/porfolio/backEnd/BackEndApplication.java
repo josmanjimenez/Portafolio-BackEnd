69fc88412ddc895e4dc5c701bd61ce942b008b5c
Josman Jimenez <jimenezjosman@gmail.com>
2023-03-25 04:37:57 -0300

    segindo intento deploy(correcion de origi cors en webconfij y modificacion de las configuracion de propertis bd)

--- src/main/java/com/porfolio/backEnd/BackEndApplication.java
+++ src/main/java/com/porfolio/backEnd/BackEndApplication.java
@@ -3,7 +3,11 @@ package com.porfolio.backEnd;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
+
 @SpringBootApplication
+
 public class BackEndApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/porfolio/backEnd/WebConfig.java
+++ src/main/java/com/porfolio/backEnd/WebConfig.java
@@ -9,12 +9,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 
-public class WebConfig  implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer {
     @Override
 
     public void addCorsMappings(CorsRegistry registry) {
 
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+        .allowedOrigins("*")
+        .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
+        .maxAge(168000)
+        .allowedHeaders("*")
+        .allowCredentials(true);
 
     }
 }
